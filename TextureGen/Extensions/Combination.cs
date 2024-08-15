96849f27736524b886f1837be3a95372e37eae73
Daniel Errington <derrington@gmail.com>
2024-08-15 21:01:16 +0100

    Add application of image as mask

---
+++ TextureGen/Extensions/Combination.cs
@@ -0,0 +1,29 @@
+﻿namespace TextureGen;
+
+public static class Combination
+{
+    public static Texture ApplyAsMask(this Texture mask, Texture texture1, Texture texture2)
+    {
+        if (texture1.ImageSize != texture2.ImageSize || texture1.ImageSize != mask.ImageSize) throw new TextureSizesDoNotMatchException();
+
+        var maskData = mask.Data.Span;
+        var texture1Data = texture1.Data.Span;
+        var texture2Data = texture2.Data.Span;
+        var resultData = new byte[texture1Data.Length];
+
+        for (var i = 0; i < resultData.Length; ++i)
+        {
+            resultData[i] =
+                maskData[i] switch
+                {
+                    0 => texture2Data[i],
+                    1 => texture1Data[i],
+                    var w => (byte)((w * texture1Data[i] + (255 - w) * texture2Data[i]) / 255)
+                };
+        }
+
+        return new(mask.ImageSize, resultData);
+    }
+
+    public class TextureSizesDoNotMatchException : ArgumentException;
+}
