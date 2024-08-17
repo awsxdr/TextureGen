b7c890f1e9072cbc481642fe1cc68cb86b89f52e
Daniel Errington <derrington@gmail.com>
2024-08-17 23:05:39 +0100

    Add gradient to rectangles

--- TextureGen/Generation/RectangleGenerator.cs
+++ TextureGen/Generation/RectangleGenerator.cs
@@ -2,7 +2,12 @@
 
 public class RectangleGenerator(ImageSize imageSize) : IGenerator<RectangleGenerator.Parameters>
 {
-    public Texture Generate(Parameters parameters)
+    public Texture Generate(Parameters parameters) =>
+        parameters.Gradient > 0f
+            ? GenerateWithGradient(parameters)
+            : GenerateWithoutGradient(parameters);
+
+    private Texture GenerateWithoutGradient(Parameters parameters)
     {
         var columnStart = (int)(imageSize * parameters.Left);
         var columnCount = Math.Min((int)(imageSize * parameters.Width), imageSize - columnStart);
@@ -21,11 +26,81 @@ public class RectangleGenerator(ImageSize imageSize) : IGenerator<RectangleGener
         return new Texture(imageSize, data);
     }
 
+    private Texture GenerateWithGradient(Parameters parameters)
+    {
+        var columnStart = (int)(imageSize * parameters.Left);
+        var columnCount = Math.Min((int)(imageSize * parameters.Width), imageSize - columnStart);
+        var rowStart = (int)(imageSize * parameters.Top);
+        var rowEnd = Math.Min((int)(imageSize * (parameters.Top + parameters.Height)), imageSize);
+
+        var data = new byte[imageSize * imageSize * Color.Size];
+        var dataSpan = data.AsSpan();
+
+        var standardRow = new byte[imageSize * Color.Size];
+        var standardRowSpan = standardRow.AsSpan();
+        var gradientWidth = (int)(parameters.Gradient * columnCount / 2);
+
+        if (gradientWidth > 0)
+        {
+            var gradientColumnStep = 255 / gradientWidth;
+
+            for (var x = 0; x < gradientWidth; ++x)
+            {
+                standardRowSpan.Slice((x + columnStart) * Color.Size, Color.Size).Fill((byte)(x * gradientColumnStep));
+                standardRowSpan.Slice((columnStart + columnCount - x - 1) * Color.Size, Color.Size)
+                    .Fill((byte)(x * gradientColumnStep));
+            }
+        }
+
+        standardRowSpan.Slice(
+            (columnStart + gradientWidth) * Color.Size,
+            (columnCount - gradientWidth * 2) * Color.Size)
+            .Fill(255);
+
+        var gradientHeight = (int)(parameters.Gradient * (rowEnd - rowStart) / 2);
+
+        if (gradientHeight > 0)
+        {
+            var gradientRowStep = 255 / gradientHeight;
+
+            for (var y = 0; y < gradientHeight; ++y)
+            {
+                var topRowSpan = dataSpan.Slice((y + rowStart) * imageSize * Color.Size, imageSize * Color.Size);
+
+                if (gradientWidth > 0)
+                {
+                    var gradientColumnStep = 255 / gradientWidth;
+                    for (var x = 0; x < gradientWidth; ++x)
+                    {
+                        var value = (byte)Math.Min(x * gradientColumnStep, y * gradientRowStep);
+                        topRowSpan.Slice((x + columnStart) * Color.Size, Color.Size).Fill(value);
+                        topRowSpan.Slice((columnStart + columnCount - x - 1) * Color.Size, Color.Size).Fill(value);
+                    }
+                }
+
+                topRowSpan.Slice((columnStart + gradientWidth) * Color.Size,
+                        (columnCount - gradientWidth * 2) * Color.Size)
+                    .Fill((byte)(gradientRowStep * y));
+
+                topRowSpan.CopyTo(dataSpan.Slice((rowEnd - y - 1) * imageSize * Color.Size, imageSize * Color.Size));
+            }
+        }
+
+        for (var row = rowStart + gradientHeight; row < rowEnd - gradientHeight; ++row)
+        {
+            var rowSpan = dataSpan.Slice(row * imageSize * Color.Size, imageSize * Color.Size);
+            standardRowSpan.CopyTo(rowSpan);
+        }
+
+        return new Texture(imageSize, data);
+    }
+
     public class Parameters
     {
         public Percentage Left { get; init; } = 0.0f;
         public Percentage Top { get; init; } = 0.0f;
         public Percentage Width { get; init; } = 1.0f;
         public Percentage Height { get; init;} = 1.0f;
+        public Percentage Gradient { get; init; } = 0.0f;
     }
 }
