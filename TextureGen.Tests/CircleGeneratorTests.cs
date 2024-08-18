ab3f55ea298762b81921a16c0db499f3e072d46f
Daniel Errington <derrington@gmail.com>
2024-08-18 15:26:04 +0100

    Add linear and curved circle generators

---
+++ TextureGen.Tests/CircleGeneratorTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using TextureGen.Generation;
+
+namespace TextureGen.Tests;
+
+public class CircleGeneratorTests
+{
+    [TestCaseSource(typeof(CommonTestCases), nameof(CommonTestCases.AllImageSizes))]
+    public void RecordSolidGeneratorPerformance(ImageSize imageSize)
+    {
+        var generator = new CircleGenerator(imageSize);
+
+        var time = Enumerable.Range(0, 10).Select(_ =>
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            generator.Generate(new CircleGenerator.Parameters { FillType = CircleFillType.Solid, Radius = 0.5f });
+
+            return stopwatch.ElapsedMilliseconds;
+        }).Average();
+
+        Console.WriteLine($"{time}ms");
+    }
+
+    [TestCaseSource(typeof(CommonTestCases), nameof(CommonTestCases.AllImageSizes))]
+    public void RecordLinearGeneratorPerformance(ImageSize imageSize)
+    {
+        var generator = new CircleGenerator(imageSize);
+
+        var time = Enumerable.Range(0, 10).Select(_ =>
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            generator.Generate(new CircleGenerator.Parameters { FillType = CircleFillType.Linear, Radius = 0.5f });
+
+            return stopwatch.ElapsedMilliseconds;
+        }).Average();
+
+        Console.WriteLine($"{time}ms");
+    }
+
+    [TestCaseSource(typeof(CommonTestCases), nameof(CommonTestCases.AllImageSizes))]
+    public void RecordCurvedGeneratorPerformance(ImageSize imageSize)
+    {
+        var generator = new CircleGenerator(imageSize);
+
+        var time = Enumerable.Range(0, 10).Select(_ =>
+        {
+            var stopwatch = Stopwatch.StartNew();
+
+            generator.Generate(new CircleGenerator.Parameters { FillType = CircleFillType.Curved, Radius = 0.5f });
+
+            return stopwatch.ElapsedMilliseconds;
+        }).Average();
+
+        Console.WriteLine($"{time}ms");
+    }
+}
--- TextureGen/Generation/CircleGenerator.cs
+++ TextureGen/Generation/CircleGenerator.cs
@@ -37,6 +37,29 @@ public class CircleGenerator(ImageSize imageSize) : IGenerator<CircleGenerator.P
         var data = new byte[imageSize * imageSize * Color.Size];
         var dataSpan = data.AsSpan();
 
+        var imageRadius = imageSize * parameters.Radius;
+        var midpoint = imageSize / 2;
+        var rowStart = (int)(midpoint - imageRadius);
+
+        for (var row = rowStart; row < midpoint; ++row)
+        {
+            var halfWidth = Math.Max(0, (int)(MathF.Sin(MathF.Acos((midpoint - row) / imageRadius)) * imageRadius));
+            var start = midpoint - halfWidth;
+
+            var rowSpan = dataSpan.Slice(row * imageSize * Color.Size, imageSize * Color.Size);
+
+            for (var column = start; column < midpoint; ++column)
+            {
+                var distanceToCenter = MathF.Sqrt(MathF.Pow(midpoint - column, 2f) + MathF.Pow(midpoint - row, 2f)) / imageRadius;
+                var colorBytes = Color.Gray((byte)(255 - Math.Min(255, distanceToCenter * 255))).ToBytes();
+                
+                colorBytes.CopyTo(rowSpan.Slice(column * Color.Size, Color.Size));
+                colorBytes.CopyTo(rowSpan.Slice((imageSize - column - 1) * Color.Size, Color.Size));
+            }
+
+            rowSpan.CopyTo(dataSpan.Slice((imageSize - row - 1) * imageSize * Color.Size));
+        }
+
         return new Texture(imageSize, data);
     }
 
@@ -44,6 +67,29 @@ public class CircleGenerator(ImageSize imageSize) : IGenerator<CircleGenerator.P
     {
         var data = new byte[imageSize * imageSize * Color.Size];
         var dataSpan = data.AsSpan();
+
+        var imageRadius = imageSize * parameters.Radius;
+        var midpoint = imageSize / 2;
+        var rowStart = (int)(midpoint - imageRadius);
+
+        for (var row = rowStart; row < midpoint; ++row)
+        {
+            var halfWidth = Math.Max(0, (int)(MathF.Sin(MathF.Acos((midpoint - row) / imageRadius)) * imageRadius));
+            var start = midpoint - halfWidth;
+
+            var rowSpan = dataSpan.Slice(row * imageSize * Color.Size, imageSize * Color.Size);
+
+            for (var column = start; column < midpoint; ++column)
+            {
+                var distanceToCenter = MathF.Sqrt(MathF.Pow(midpoint - column, 2f) + MathF.Pow(midpoint - row, 2f));
+                var colorBytes = Color.Gray((byte)(255 - Math.Min(255, 255 - Math.Cos(Math.Asin(distanceToCenter / imageRadius)) * 255))).ToBytes();
+
+                colorBytes.CopyTo(rowSpan.Slice(column * Color.Size, Color.Size));
+                colorBytes.CopyTo(rowSpan.Slice((imageSize - column - 1) * Color.Size, Color.Size));
+            }
+
+            rowSpan.CopyTo(dataSpan.Slice((imageSize - row - 1) * imageSize * Color.Size));
+        }
 
         return new Texture(imageSize, data);
     }
